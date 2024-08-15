e07344183389a101483085475b013dcb49e7c7bc
pejinovics <pejinovicstefan@gmail.com>
2024-08-15 14:21:51 +0200

    feat: add User model

---
+++ src/main/java/com/onlineMarketplace/project/model/User.java
@@ -0,0 +1,36 @@
+package com.onlineMarketplace.project.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false, columnDefinition = "DATE")
+    private LocalDate registrationDate;
+}
