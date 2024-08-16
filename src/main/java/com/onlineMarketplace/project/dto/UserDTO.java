92379e238d49442a20af9fd325d6150afd8e80da
pejinovics <pejinovicstefan@gmail.com>
2024-08-16 16:44:26 +0200

    fix: add getters, setters and constructors

--- src/main/java/com/onlineMarketplace/project/dto/UserDTO.java
+++ src/main/java/com/onlineMarketplace/project/dto/UserDTO.java
@@ -1,9 +1,17 @@
 package com.onlineMarketplace.project.dto;
 
 import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
     private Long id;
     private String username;
