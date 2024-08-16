0e8163241cb1930221a8bc78b3754cca7a1551f5
pejinovics <pejinovicstefan@gmail.com>
2024-08-16 16:45:00 +0200

    feat: add interface for user service

--- src/main/java/com/onlineMarketplace/project/service/interfaces/IUserService.java
+++ src/main/java/com/onlineMarketplace/project/service/interfaces/IUserService.java
@@ -1,4 +1,15 @@
 package com.onlineMarketplace.project.service.interfaces;
 
+import com.onlineMarketplace.project.dto.UserDTO;
+import com.onlineMarketplace.project.model.User;
+
+import java.util.Collection;
+import java.util.Optional;
+
 public interface IUserService {
+    Collection<User> findAll();
+    Optional<User> findById(Long id);
+    User registerUser(UserDTO userDTO);
+    User save(User user);
+    void deleteById(Long id);
 }
