0a57387b03fcf7e2c9115e210b47c7e89f2999b1
pejinovics <pejinovicstefan@gmail.com>
2024-08-16 16:45:50 +0200

    feat: add interface for advertisement service

--- src/main/java/com/onlineMarketplace/project/service/interfaces/IAdvertisementService.java
+++ src/main/java/com/onlineMarketplace/project/service/interfaces/IAdvertisementService.java
@@ -1,4 +1,21 @@
 package com.onlineMarketplace.project.service.interfaces;
 
+import com.onlineMarketplace.project.dto.AdvertisementCardDTO;
+import com.onlineMarketplace.project.dto.AdvertisementDTO;
+import com.onlineMarketplace.project.model.Advertisement;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Optional;
+
 public interface IAdvertisementService {
+
+    Collection<AdvertisementDTO> findAll();
+    Collection<AdvertisementCardDTO> findAllCards();
+    AdvertisementDTO findById(Long id);
+    Advertisement save(Advertisement advertisement) ;
+    void deleteById(Long id);
+    AdvertisementDTO create(AdvertisementDTO advertisementDTO);
+    Optional<AdvertisementDTO> change(AdvertisementDTO advertisementDTO, Long id);
+
 }
