83e61441661c4c4e0cc27997d4fcbd0449ab16bf
pejinovics <pejinovicstefan@gmail.com>
2024-08-18 18:43:54 +0200

    feat: add query for filter advertisements

--- src/main/java/com/onlineMarketplace/project/repository/IAdvertisementRepository.java
+++ src/main/java/com/onlineMarketplace/project/repository/IAdvertisementRepository.java
@@ -2,6 +2,23 @@ package com.onlineMarketplace.project.repository;
 
 import com.onlineMarketplace.project.model.Advertisement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
 
 public interface IAdvertisementRepository extends JpaRepository<Advertisement, Long> {
+    @Query("SELECT a FROM Advertisement a " +
+            "JOIN a.categories c " +
+            "WHERE (:category IS NULL OR c = :category) " +
+            "AND (:titleContains IS NULL OR LOWER(a.title) LIKE LOWER(CONCAT('%', :titleContains, '%'))) " +
+            "AND (:userId = 0 OR a.user.id = :userId) " +
+            "ORDER BY " +
+            "CASE WHEN :sortBy = 'asc' THEN a.price END ASC, " +
+            "CASE WHEN :sortBy = 'desc' THEN a.price END DESC"
+    )
+    Collection<Advertisement> filterAdvertisements(@Param("category") String category,
+                                                   @Param("titleContains") String titleContains,
+                                                   @Param("userId") Long userId,
+                                                   @Param("sortBy") String sortBy);
 }
